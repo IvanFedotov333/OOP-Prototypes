97524a671763798dce39029c9d1d3e651027d698
IvanFedotov333 <ivan_fedotov_2013@inbox.ru>
2026-07-29 13:26:23 +0300

    ДЗ№15

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+const DomElement = function (selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+
+  this.createElement = function () {
+    let element;
+    if (this.selector.startsWith(".")) {
+      element = document.createElement("div");
+      element.className = this.selector.slice(1);
+    } else if (this.selector.startsWith("#")) {
+      element = document.createElement("p");
+      element.id = this.selector.slice(1);
+    } else {
+      element = document.createElement("div");
+    }
+    element.style.cssText = `
+height: ${this.height};
+width: ${this.width};
+background:${this.bg};
+font-size:${this.fontSize}`;
+
+    element.textContent = "Hello!";
+    return element;
+  };
+};
+const newElement = new DomElement(
+  ".my-block",
+  "100px",
+  "200px",
+  "green",
+  "18px",
+);
+
+const createdElement = newElement.createElement();
+
+document.body.append(createdElement);
